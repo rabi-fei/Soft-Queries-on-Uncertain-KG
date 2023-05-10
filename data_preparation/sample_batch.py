9afab23cb100e39ad9e4d54f7ef4c52a77e881fc
Edwin-yin <yinh-wx@outlook.com>
2023-05-10 18:38:52 +0800

    Update sample_batch.py

--- data_preparation/sample_batch.py
+++ data_preparation/sample_batch.py
@@ -1,11 +1,21 @@
 import os
+import argparse
 
-each_num = 5
-dataset = 'NELL'
-for start in range(0, 150, each_num):
-    command = ("nohup python data_preparation/sample_query.py "
-               f"--start_index {start} "
-               f"--end_index {start + each_num - 1} > sample_EFOX_{dataset}_{start}_{start + each_num - 1}.log 2>&1 &")
-    print(command, '\n')
-    os.system(command)
-    print("launched")
+parser = argparse.ArgumentParser()
+parser.add_argument("--start", type=int, default=0)
+parser.add_argument("--end", type=int, default=150)
+parser.add_argument("--dataset", type=str, default='NELL')
+parser.add_argument("--each", type=int, default=5)
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    dataset = args.dataset
+    each_num = args.each
+    for start in range(args.start, args.end, each_num):
+        command = ("nohup python data_preparation/sample_query.py "
+                   f"--start_index {start} "
+                   f"--end_index {start + each_num - 1} > sample_EFOX_{dataset}_{start}_{start + each_num - 1}.log 2>&1 &")
+        print(command, '\n')
+        os.system(command)
+        print("launched")
+
